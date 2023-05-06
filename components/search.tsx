9acbbe327e794efac53da463a2f11a75620dedf4
Ken Sasongko <ken.sasongko@gmail.com>
2023-05-07 00:14:00 +0700

    feat(search): enter on input text to submit

--- components/search.tsx
+++ components/search.tsx
@@ -12,9 +12,13 @@ export const SearchForm = (props: SearchFormProps) => {
   const [searchString, setSearchString] = useState(props.search);
 
   return (
-    <form method="get">
+    <form method="get" id="searchForm">
       <p className="flex w-full justify-center border-b border-gray-300 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-4" type="text" name="search" placeholder="Search" value={searchString} onChange={(e) => setSearchString(e.target.value)}/>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-4" type="text" name="search" placeholder="Search" value={searchString} onChange={(e) => setSearchString(e.target.value)} onKeyPress={(e) => {
+    if(e.key === 'Enter') {
+      (document.getElementById('searchForm')! as HTMLFormElement).submit();
+    }
+  }}/>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
           Search
         </button>
